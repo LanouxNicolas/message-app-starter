f6b22f00031abf208de763ce35ce3dbf45194b49
LanouxNicolas <nicolas.lanoux@etu.univ-cotedazur.fr>
2020-11-04 17:09:00 +0100

    etape2

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="app">
-    <HelloWorld :msg="'Welcome to Your Vue.JS App'"/>
+    <Menu :unreadMessages="42"/>
+    <Messages/>
+    <Message/>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Menu from "./components/Menu.vue";
+import Messages from "./components/Messages.vue";
+import Message from "./components/Message.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Menu,
+    Messages,
+    Message
   }
 };
 </script> 
